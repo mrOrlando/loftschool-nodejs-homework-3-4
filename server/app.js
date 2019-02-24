9aecd3e6cbeebeb2254e0f68899e052f9781c7d6
mrOrlando <juriy88@mail.com>
2019-02-24 23:15:38 +0300

    Added routers and controllers.

--- server/app.js
+++ server/app.js
@@ -2,25 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+// view engine setup
+app.set('views', './server/views');
+app.set('view engine', 'pug');
+
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('views', './server/views');
-app.set('view engine', 'pug');
-
-app.get('/', function(req, res) {
-  res.render('pages/index');
-});
-
-app.get('/login', function(req, res) {
-  res.render('pages/login');
-});
-
-app.get('/admin', function(req, res) {
-  res.render('pages/admin');
-});
+app.use('/', require('./routes/index'));
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
