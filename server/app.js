08f7292d4c73266fcde5deb072e6ea1cdaef4531
mrOrlando <juriy88@mail.com>
2019-02-26 22:42:17 +0300

    Added setting for application port.

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 
 // view engine setup
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/', require('./routes/index'));
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`);
 });
