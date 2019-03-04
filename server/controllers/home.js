980b53a94bea625e7d3a6fcbd43dbe51e8f9fd1d
Yury Karaliuk <yury.karaliuk@itechart-group.com>
2019-03-04 11:29:03 +0300

    Updated messages during an email sending.

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -6,18 +6,18 @@ module.exports.get = function(req, res) {
   const data = {
     skillsYears: db.get('skills').value(),
     products: db.get('products').value(),
+    msgemail: req.flash('info')[0],
   };
 
   res.render('pages/index', data);
 };
 
 module.exports.post = function(req, res) {
-  // требуем наличия имени, обратной почты и текста
   if (!req.body.name || !req.body.email || !req.body.message) {
-    // если что-либо не указано - сообщаем об этом
-    return res.json({ msg: 'Все поля нужно заполнить!', status: 'Error' });
+    req.flash('info', 'Нужно заполнить все поля!');
+    res.redirect('/');
   }
-  // инициализируем модуль для отправки писем и указываем данные из конфига
+
   const transporter = nodemailer.createTransport(config.mail.smtp);
   const mailOptions = {
     from: `"${req.body.name}" <${req.body.email}>`,
@@ -27,15 +27,14 @@ module.exports.post = function(req, res) {
       req.body.message.trim().slice(0, 500) +
       `\n Отправлено с: <${req.body.email}>`,
   };
-  // отправляем почту
-  transporter.sendMail(mailOptions, function(error, info) {
-    // если есть ошибки при отправке - сообщаем об этом
+
+  transporter.sendMail(mailOptions, function(error) {
     if (error) {
-      return res.json({
-        msg: `При отправке письма произошла ошибка!: ${error}`,
-        status: 'Error',
-      });
+      req.flash('info', 'При отправке письма произошла ошибка!');
+      res.redirect('/');
     }
-    res.json({ msg: 'Письмо успешно отправлено!', status: 'Ok' });
+
+    req.flash('info', 'Письмо успешно отправлено!');
+    res.redirect('/');
   });
 };
